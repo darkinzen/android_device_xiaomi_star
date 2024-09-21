b5ad1b70a767d7d8d82fcbcbb7ae1b954c0166ba
Alexander Winkowski <dereference23@outlook.com>
2024-09-21 16:14:19 +0000

    miatoll: Added sepolicy for MiuiCamera
    Co-authored-by: Arian <arian.kulmer@web.de>
    Co-authored-by: COSMIC <rayavanindra@gmail.com>
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,5 +38,56 @@ TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 # Recovery
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 165
 
+<<<<<<< HEAD
 # Include proprietary files
 include vendor/xiaomi/star/BoardConfigVendor.mk
+=======
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
+
+# RIL
+ENABLE_VENDOR_RIL_SERVICE := true
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2023-05-01
+
+# Sepolicy
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
+include $(DEVICE_PATH)/sepolicy/dolby/SEPolicy.mk
+include $(DEVICE_PATH)/sepolicy/misys/SEPolicy.mk
+include $(DEVICE_PATH)/sepolicy/miuicamera/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+BOARD_AVB_VBMETA_SYSTEM := system system_ext product
+BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 1
+
+# WiFi
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+QC_WIFI_HIDL_FEATURE_DUAL_AP := true
+WIFI_DRIVER_DEFAULT := qca_cld3
+WIFI_DRIVER_STATE_CTRL_PARAM := "/dev/wlan"
+WIFI_DRIVER_STATE_OFF := "OFF"
+WIFI_DRIVER_STATE_ON := "ON"
+WIFI_HIDL_FEATURE_AWARE := true
+WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
+WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+
+# Inherit proprietary blobs
+include vendor/xiaomi/miatoll/BoardConfigVendor.mk
+>>>>>>> b7c18a7 (miatoll: Added sepolicy for MiuiCamera)
