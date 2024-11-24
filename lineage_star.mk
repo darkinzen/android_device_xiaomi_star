53f60c6c8eafab75085475693d1381c94bde6dc0
Darkinzen <h.bilgic1818@gmail.com>
2024-11-24 16:28:15 +0300

    Add RisingOS related stuffs

--- lineage_star.mk
+++ lineage_star.mk
@@ -23,6 +23,29 @@ PRODUCT_NAME := lineage_star
 PRODUCT_SYSTEM_NAME := star_global
 PRODUCT_SYSTEM_DEVICE := star
 
+# For RisingOS Build
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	RisingMaintainer="Darkinzen"
+
+RISING_MAINTAINER="Darkinzen" #Display Name on bash
+TARGET_ENABLE_BLUR := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# extra add-ons for core GMS builds
+# List of add-ons
+ PRODUCT_PACKAGES += \
+    Photos \
+    AiWallpapers \
+    WallpaperEmojiPrebuilt \
+    CalculatorGooglePrebuilt \
+    CalendarGooglePrebuilt \
+
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="star_global-user 14 UKQ1.231207.002 V816.0.6.0.UKAMIXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
