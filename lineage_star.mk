7edd70dbde16c14a15cd5dae65bafa5d2085fe27
Darkinzen <h.bilgic1818@gmail.com>
2024-11-24 15:13:22 +0300

    star: convert from mars

--- lineage_star.mk
+++ lineage_star.mk
@@ -8,25 +8,25 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from mars device
-$(call inherit-product, device/xiaomi/mars/device.mk)
+# Inherit from star device
+$(call inherit-product, device/xiaomi/star/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Xiaomi
-PRODUCT_DEVICE := mars
+PRODUCT_DEVICE := star
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2102K1AC
-PRODUCT_NAME := lineage_mars
+PRODUCT_NAME := lineage_star
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-PRODUCT_SYSTEM_NAME := mars_global
-PRODUCT_SYSTEM_DEVICE := mars
+PRODUCT_SYSTEM_NAME := star_global
+PRODUCT_SYSTEM_DEVICE := star
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="mars_global-user 14 UKQ1.231207.002 V816.0.6.0.UKAMIXM release-keys" \
+    PRIVATE_BUILD_DESC="star_global-user 14 UKQ1.231207.002 V816.0.6.0.UKAMIXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := Xiaomi/mars_global/mars:14/UKQ1.231207.002/V816.0.6.0.UKAMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/star_global/star:14/UKQ1.231207.002/V816.0.6.0.UKAMIXM:user/release-keys
