4dc77e2605acbeda66778edaebdf9f74eed963aa
Darkinzen <h.bilgic1818@gmail.com>
2024-11-24 15:23:07 +0300

    Enable building broken dup rules and build prebuilt elf files

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,10 @@ DEVICE_PATH := device/xiaomi/star
 # Inherit from sm8350-common
 include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 
+
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_DUP_RULES := true
+
 # Board
 TARGET_BOOTLOADER_BOARD_NAME := star
 
